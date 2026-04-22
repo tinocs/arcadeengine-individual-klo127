d4aad03e977e0c6fefe99afb9f3e02054480f4fe
Kyle Lo <klo127@student.fuhsd.org>
2026-04-21 17:44:27 -0700

    Added World and BallWorld

---
+++ src/breakout/Ball.java
@@ -0,0 +1,27 @@
+package breakout;
+
+import engine.Actor;
+import javafx.scene.image.Image;
+
+public class Ball extends Actor {
+    private int dx;
+    private int dy;
+    Image image;
+
+    public Ball(){
+        image = new Image("file:ball.png");
+        dx = 5;
+        dy = 5;
+        setImage(image);
+    }
+    @Override
+    public void act(long now) {
+        move(dx,dy);
+        if(getX()<=0||(getX()+getWidth())>getWorld().getWidth()){
+            dx =-dx;
+        }
+        if(getY()<=0||(getY()+getHeight())>getWorld().getHeight()){
+            dy =-dy;
+        }
+    }
+}
