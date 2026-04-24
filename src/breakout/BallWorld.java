31c906fd5214bd67d313548f8507a5f6b37d4d1c
Kyle Lo <klo127@student.fuhsd.org>
2026-04-24 15:14:46 -0700

    Added Levels and Title

--- src/breakout/BallWorld.java
+++ src/breakout/BallWorld.java
@@ -2,19 +2,30 @@ package breakout;
 
 import engine.World;
 import javafx.event.EventHandler;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 
 public class BallWorld extends World {
     private Paddle paddle;
     private Score score;
+    int level;
     public BallWorld() {
-        setPrefSize(800,600);
+        setPrefSize(700,500);
+        level = 1;
 
     }
 
     @Override
     public void act(long now) {
-
+        if(getObjects(Brick.class).isEmpty()){
+            if(level ==1){
+                createBrickRect(3,3);
+                level++;
+            }else if(level==2){
+                createBrickRect(6,7);
+                level++;
+            }
+        }
     }
 
     @Override
@@ -32,29 +43,37 @@ public class BallWorld extends World {
         paddle = new Paddle();
         add(paddle);
         paddle.setX(getWidth()/2-paddle.getWidth()/2);
-        paddle.setY(getHeight()*7/8-paddle.getHeight()/2);
+        paddle.setY(getHeight()*9/10-paddle.getHeight()/2);
         this.setOnMouseMoved(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
                 paddle.setX(event.getX()-paddle.getWidth()/2);
             }
         });
-        int rows=5;
-        int cols=10;
+
+    }
+    public Score getScore(){
+        return score;
+    }
+    public void createBrickRect(int rows, int cols){
+
         Brick b = new Brick();
         double x=(getWidth()-cols*b.getWidth())/2;
         double y=getHeight()/8;
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
+
                 Brick brick=new Brick();
-                add(brick);
+
                 brick.setX(j*b.getWidth()+x);
                 brick.setY(i*b.getHeight()+y);
+                if(i==0||i==rows-1||j==0||j==cols-1){
+                    brick.setImage(new Image("/breakoutresources/brick2.png"));
+                }
+                add(brick);
             }
         }
     }
-    public Score getScore(){
-        return score;
-    }
 }
+
 
--- src/breakout/Breakout.java
+++ src/breakout/Breakout.java
@@ -2,22 +2,48 @@ package breakout;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Breakout extends Application {
+    Scene levels;
+    BallWorld world;
     public static void main(String[] args) {
         launch(args);
     }
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Breakout");
-        BorderPane root = new BorderPane();
-        BallWorld world = new BallWorld();
-        root.setCenter(world);
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        world.start();
+
+        Pane menuRoot = new Pane();
+        Button playButton = new Button("Play!");
+        playButton.setFont(new Font(20));
+        playButton.setPrefWidth(200);
+        playButton.setOnAction(event -> {
+            stage.setScene(levels);
+            world.start();
+        });
+        Image menuImage = new Image("/breakoutresources/menu.png",700,500,false,true);
+        ImageView menuBackground = new ImageView(menuImage);
+        playButton.setLayoutX(menuImage.getWidth()/2-120);
+        playButton.setLayoutY(menuImage.getHeight()*0.82-10);
+        menuRoot.getChildren().addAll(menuBackground,playButton);
+
+        BorderPane levelOneRoot = new BorderPane();
+        world = new BallWorld();
+        levelOneRoot.setCenter(world);
+        levels = new Scene(levelOneRoot);
+        stage.setScene(levels);
+
+
+        Scene mainMenu = new Scene(menuRoot);
+        stage.setScene(mainMenu);
         stage.show();
     }
 }
+
